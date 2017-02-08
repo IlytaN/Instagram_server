e883fb3d94943ad312b73c6a854266eadcad43d5
Ilyta N <snowprincess2305@yahoo.com>
2017-02-08 18:16:37 +0200

    edit server file

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var app = express();
 // You can store key-value pairs in express, here we store the port setting
 var express = require('express'),
 app = express();
-app.use(express.static('www'));
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
