b86da93ea4bc17768d30affffdf9abcc1967c8d1
Ilyta N <snowprincess2305@yahoo.com>
2017-02-09 15:25:43 +0200

    edit a little

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var posts = require('./home/Posts.js');
 app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(bodyParser.json());
-// app.get('/', function(req, res) {
-//     res.send("Hello world");
-// });
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
